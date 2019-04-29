733643943b5c396f0d3b47056f6bd03bcdc33dc5
FidyBack <abel.cavalcante.ismart@gmail.com>
2019-04-28 23:59:46 -0300

    Melhoras

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MainActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MainActivity.java
@@ -1,10 +1,10 @@
 package br.pro.hashi.ensino.desagil.projeto1;
 
 import android.Manifest;
-import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // ==== Lista de Palavras ====
 
         TextView palavraTela = findViewById(R.id.text_mostrado);
         Button buttonDia = findViewById(R.id.button_bom_dia);
--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/SMSActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/SMSActivity.java
@@ -1,15 +1,31 @@
 package br.pro.hashi.ensino.desagil.projeto1;
 
+import android.content.Intent;
 import android.provider.Telephony;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.PhoneNumberUtils;
 import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class SMSActivity extends AppCompatActivity {
+    private ListView listView;
+
+    private String[] nomeArray = {"Abel","Rodrigo","Thiago","Luvi","Roger"};
+
+    private String[] contatoArray = {
+            "11946225498",
+            "11962861545",
+            "7191670735",
+            "11991237171",
+            "12982426063"
+    };
 
     private void showToast(String text){
         Toast toast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
@@ -22,26 +38,32 @@ public class SMSActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sms);
 
+        CustomListAdapter whatever = new CustomListAdapter(this, nomeArray, contatoArray);
+        listView = findViewById(R.id.listview_Android_Contacts);
+        listView.setAdapter(whatever);
+
         EditText textMassage = findViewById(R.id.text_message);
-        Button buttonContact = findViewById(R.id.button_contact);
 
-        buttonContact.setOnClickListener((view) -> {
-            String message = textMassage.getText().toString();
 
-            if (message.isEmpty()) {
-                showToast("Mensagem inválida!");
-                return;
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String message = textMassage.getText().toString();
+                String phone = contatoArray[position];
+
+                if (message.isEmpty()) {
+                    showToast("Mensagem inválida!");
+                    return;
+                }
+
+                if (!PhoneNumberUtils.isGlobalPhoneNumber(phone)) {
+                    showToast("Número inválido!");
+                    return;
+                }
+
+                SmsManager manager = SmsManager.getDefault();
+                manager.sendTextMessage(phone, null, message, null, null);
             }
-
-            String phone = buttonContact.getText().toString();
-
-            if (!PhoneNumberUtils.isGlobalPhoneNumber(phone)) {
-                showToast("Número inválido!");
-                return;
-            }
-
-            SmsManager manager = SmsManager.getDefault();
-            manager.sendTextMessage(phone, null, message, null, null);
         });
     }
 }
