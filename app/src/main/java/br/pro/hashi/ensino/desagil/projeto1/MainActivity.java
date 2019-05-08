bfe5deb683d59fdf8a4120f6069ad67ba50ee74a
RogerPina <rogerrfp@al.insper.edu.br>
2019-05-08 20:33:16 -0300

    Mudei a main

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MainActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MainActivity.java
@@ -18,12 +18,8 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int REQUEST_SEND_SMS = 0;
-
-    private void startsSMSActivity(){
-        Intent intent = new Intent(this, SMSActivity.class);
-        TextView palavraTela = findViewById(R.id.text_mostrado);
-        intent.putExtra("palavra", palavraTela.getText().toString());
+    private void startsMessageActivity(){
+        Intent intent = new Intent(this, MessageActivity.class);
         startActivity(intent);
     }
 
@@ -32,63 +28,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // ==== Lista de Palavras ====
+        Button buttonMessageActivity = findViewById(R.id.button_goMessageActivity);
 
-        TextView palavraTela = findViewById(R.id.text_mostrado);
-        Button buttonDia = findViewById(R.id.button_bom_dia);
-        Button buttonNoite = findViewById(R.id.button_boa_noite);
-        Button buttonAgua = findViewById(R.id.buttom_agua);
-        Button buttonComida = findViewById(R.id.buttom_comida);
-        Button buttonDormir = findViewById(R.id.buttom_dormir);
-
-        buttonDia.setOnClickListener((view) -> {
-            String content = buttonDia.getText().toString();
-            palavraTela.setText(palavraTela.getText().toString() + " " + content);
+        buttonMessageActivity.setOnClickListener((view) -> {
+            startsMessageActivity();
         });
-
-        buttonNoite.setOnClickListener((view) -> {
-            String content = buttonNoite.getText().toString();
-            palavraTela.setText(palavraTela.getText().toString() + " " + content);
-        });
-
-        buttonAgua.setOnClickListener((view) -> {
-            String content = buttonAgua.getText().toString();
-            palavraTela.setText(palavraTela.getText().toString() + " " + content);
-        });
-
-        buttonComida.setOnClickListener((view) -> {
-            String content = buttonComida.getText().toString();
-            palavraTela.setText(palavraTela.getText().toString() + " " + content);
-        });
-
-        buttonDormir.setOnClickListener((view) -> {
-            String content = buttonDormir.getText().toString();
-            palavraTela.setText(palavraTela.getText().toString() + " " + content);
-        });
-
-        // ==== SMS ====
-
-        Button buttonPermissionSMS = findViewById(R.id.button_getPermissionSMS);
-
-        buttonPermissionSMS.setOnClickListener((view) -> {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED) {
-                startsSMSActivity();
-            } else {
-
-                String[] permissions = new String[]{
-                        Manifest.permission.SEND_SMS,
-                };
-
-                ActivityCompat.requestPermissions(this, permissions, REQUEST_SEND_SMS);
-            }
-        });
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == REQUEST_SEND_SMS && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            startsSMSActivity();
-        }
     }
 }
 
