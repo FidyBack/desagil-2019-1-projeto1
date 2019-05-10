a5e36ae87ac82c5a8a6a9e08b38d51d0907b21dd
RogerPina <rogerrfp@al.insper.edu.br>
2019-05-10 15:22:21 -0300

    fb

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/SMSActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/SMSActivity.java
@@ -2,6 +2,7 @@ package br.pro.hashi.ensino.desagil.projeto1;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -9,12 +10,15 @@ import android.os.Bundle;
 import android.telephony.PhoneNumberUtils;
 import android.telephony.SmsManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseException;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
@@ -44,16 +48,15 @@ public class SMSActivity extends AppCompatActivity implements ValueEventListener
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sms);
 
-
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, nomesContatos);
 
         listView = findViewById(R.id.listview_Android_Contacts);
         listView.setAdapter(arrayAdapter);
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference frases = database.getReference("Contatos");
+        DatabaseReference contatosFBReference = database.getReference("Contatos");
 
-        frases.addChildEventListener(new ChildEventListener() {
+        contatosFBReference.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
                 String key = dataSnapshot.getKey();
@@ -80,37 +83,67 @@ public class SMSActivity extends AppCompatActivity implements ValueEventListener
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
-          
-          
-            Button buttonMainActivity = findViewById(R.id.button_goMainActivity);
-            buttonMainActivity.setOnClickListener((view) -> startsMainActivity());
+        });
 
-            CustomListAdapter whatever = new CustomListAdapter(this, nomeArray, contatoArray);
-            ListView listView = findViewById(R.id.listview_Android_Contacts);
-            listView.setAdapter(whatever);
+        Button buttonSendMessage = findViewById(R.id.button_sendMessage);
 
-            TextView textMassage = findViewById(R.id.text_mostrado);
-            Bundle extras = getIntent().getExtras();
-            String message = extras.getString("palavrasms");
-            textMassage.setText(message);
 
-            listView.setOnItemClickListener((parent, view, position, id) -> {
-                String message1 = textMassage.getText().toString();
-                String phone = contatoArray[position];
+        Button buttonMainActivity = findViewById(R.id.button_goMainActivity);
+        buttonMainActivity.setOnClickListener((view) -> startsMainActivity());
 
-                if (message1.isEmpty()) {
-                    showToast("Mensagem inválida!");
-                    return;
+        TextView textMassage = findViewById(R.id.text_mostrado);
+        Bundle extras = getIntent().getExtras();
+        String message = extras.getString("palavrasms");
+        textMassage.setText(message);
+
+        EditText destinatario = findViewById(R.id.destinatario);
+
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            DatabaseReference phone = contatosFBReference.child(nomesContatos.get(position));
+
+            phone.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    String text;
+                    try {
+                        // O método getValue recebe como parâmetro uma
+                        // classe Java que representa o tipo de dado
+                        // que você acredita estar lá. Se você errar,
+                        // esse método vai lançar uma DatabaseException.
+                        text = dataSnapshot.getValue().toString();
+                    } catch (DatabaseException exception) {
+                        text = "Failed to parse value";
+                    }
+                    destinatario.setText(text);
                 }
 
-                if (!PhoneNumberUtils.isGlobalPhoneNumber(phone)) {
-                    showToast("Número inválido!");
-                    return;
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+        });
+
+        buttonSendMessage.setOnClickListener((view) -> {
+            String message1 = textMassage.getText().toString();
+
+            if (message1.isEmpty()) {
+                showToast("Mensagem inválida!");
+                return;
+            }
+
+            String phone = destinatario.getText().toString();
+
+            if (!PhoneNumberUtils.isGlobalPhoneNumber(phone)) {
+                showToast("Número inválido!");
+                return;
+            }
 
             SmsManager manager = SmsManager.getDefault();
             manager.sendTextMessage(phone, null, message1, null, null);
-        });
 
+            destinatario.setText("");
+        });
     }
 
     @Override
